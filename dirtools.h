19ba7109485220ce52cef1a3c77b8ab296efeb7d
nckgriva <nckgriva208@gmail.com>
2015-05-11 17:28:37 +0300

    Code reformat, cleanup and CMake.

--- dirtools.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIRTOOLS_HPP_INCLUDED
-#define DIRTOOLS_HPP_INCLUDED
-#include <vector>
-#include <string>
-
-std::vector<std::string> GetFilesOfFormat(std::string folder, std::string format="");
-bool DirectoryExists(std::string path);
-
-#endif // DIRTOOLS_HPP_INCLUDED
--- modelLoader/compileParser.sh
+++
@@ -1,3 +0,0 @@
-flexc++ -l objScanner.cpp -i objScanner.impl.h -c objScanner.h -b objScanner.base.h objScanner.l
-bisonc++ -p objParser.cpp -i objParser.impl.h -c objParser.h -b objParser.base.h objParser.y
-
